3a4330d7195daef73c0bb4214e3953b7b0f9beb9
ArzoRaza <ccoding76@gmail.com>
2024-05-18 01:03:48 +0530

    all topics has been coverd!!

--- 01_day_LearnBackend/script.js
+++ 01_day_LearnBackend/script.js
@@ -1,10 +1,45 @@
 const fs = require('fs');
 
-// writeFile 
+// ******************************* writeFile 
+/*
 fs.writeFile("firstFile.txt", "Hello bhaiyo kaise how agar aap file check kar rahe ho to main bata du ki isme kuch bhi aisa khas baat nhi likha hua h to apko janna chahiye", function(err){
     if(err) console.error(err.message);
     else console.log("Process has been complited!!")
 } )
+*/
+
+// ******************************* appendFile
+/*
+fs.appendFile("firstFile.txt", "(These Text appended: abcdefchijklmnopqrstuvwxyz)", function(err){
+    if(err) console.error(err.message);
+    else console.log("Process has been complited!!")
+} )
+*/
+
+
+// *******************************  rename
+/*
+fs.rename("firstFile.txt", "secondFile.txt", function(err){
+    if(err) console.error(err.message);
+    else console.log("Process has been complited!!")
+} )
+*/
+
+// ******************************  copyfile
+/*
+fs.copyFile("secondFile.txt", "basanti_Biography.txt", function(err){
+    if(err) console.error(err.message);
+    else console.log("Process has been complited!!")
+} )
+*/
+
+// ***************************** unlink
+/*
+fs.unlink("secondFile.txt", function(err){
+    if(err) console.error(err.message);
+    else console.log("Process has been complited!!")
+} )
+*/
 
 
 // fs.rmdir("./expFolder", {recursive: true},(err) => {
