46053f0a58a5959f0b648b58af75ef8ad72caa93
ArzoRaza <ccoding76@gmail.com>
2024-05-19 20:11:53 +0530

    day three to learning backend

--- 03_day_LearnBackend/script.js
+++ 03_day_LearnBackend/script.js
@@ -1,8 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World Vivek G AND how are you feeling today')
+app.use(function(req, res, next){
+    console.log("Chala re chala mai piya ke ghar !!");
+    next();
+});
+
+app.use(function(req, res, next){
+    console.log("you have to do super hard work ");
+    next();
 })
 
-app.listen(3000)
+app.get('/', function(req, res){
+    res.send("Akbar kaisa chal raha hai padhai");
+})
+
+app.get("/about", function(req, res, next){
+    return next(new Error("Error again"))
+})
+
+app.use(function(err, req, res, next){
+    console.log(err.stack);
+    res.status(500).send("kuch to kharabi chal raha h pith piche ?? ")
+})
+
+app.listen(3000);
